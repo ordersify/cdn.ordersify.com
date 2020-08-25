b752b935c7e05304e60d493b701249f53f422339
Andy Doan <62533868+imandydoan@users.noreply.github.com>
2020-08-25 19:18:22 +0700

    Update service-worker.js

--- stock-alerts/service-worker.js
+++ stock-alerts/service-worker.js
@@ -15,10 +15,8 @@ messaging.setBackgroundMessageHandler(function (payload) {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
   const notificationTitle = 'Background Message Title';
   const notificationOptions = {
-    body: 'Background Message body.',
-    icon: '/firebase-logo.png'
+    body: 'Background Message body.'
   };
 
-  return self.registration.showNotification(notificationTitle,
-    notificationOptions);
+  return self.registration.showNotification(notificationTitle, notificationOptions);
 });
