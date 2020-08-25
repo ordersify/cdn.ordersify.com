0d1d313da41f3d3aa045811a545257818185aa6e
Andy Doan <62533868+imandydoan@users.noreply.github.com>
2020-08-25 17:28:34 +0700

    Add files via upload

---
+++ stock-alerts/service-worker.js
@@ -0,0 +1,24 @@
+importScripts('https://www.gstatic.com/firebasejs/7.18.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/7.18.0/firebase-messaging.js');
+firebase.initializeApp({
+  apiKey: "AIzaSyCu-Ojut-fLbvAI2rIO-nKFzbKOPrQDCHs",
+  authDomain: "ordersify-stock-alerts.firebaseapp.com",
+  databaseURL: "https://ordersify-stock-alerts.firebaseio.com",
+  projectId: "ordersify-stock-alerts",
+  storageBucket: "ordersify-stock-alerts.appspot.com",
+  messagingSenderId: "929892195251",
+  appId: "1:929892195251:web:7aaea3db26975bfdec49c6",
+  measurementId: "G-G8JRKKBCGW"
+});
+const messaging = firebase.messaging();
+messaging.setBackgroundMessageHandler(function (payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  const notificationTitle = 'Background Message Title';
+  const notificationOptions = {
+    body: 'Background Message body.',
+    icon: '/firebase-logo.png'
+  };
+
+  return self.registration.showNotification(notificationTitle,
+    notificationOptions);
+});
